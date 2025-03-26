93ad68d67d871db6dad410b7c4fee603696de0f8
Vincent Nguyen <phucvn166@gmail.com>
2025-03-26 14:19:00 -0400

    Added Fredericton to University of Waterloo Google Maps link

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
       <div className="prose prose-neutral dark:prose-invert">
         <p>
           Originally from <a href="https://www.google.com/maps/place/Fredericton,+NB/@45.9454324,-66.7486524,18635m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4ca4220ba498fb2b:0xe7de2f297a415db4!8m2!3d45.9635895!4d-66.6431151!16zL20vMDJ3NzA!5m2!1e1!1e4?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D" target="_blank">Fredericton, New Brunswick</a>
-          . I'm currently studying Computer Science @ <a href="https://www.google.com/maps/place/University+of+Waterloo/@43.4722854,-80.5474325,1216m/data=!3m2!1e3!4b1!4m6!3m5!1s0x882bf6ad02edccff:0xdd9df23996268e17!8m2!3d43.4722854!4d-80.5448576!16zL20vMGpwa3c!5m2!1e4!1e1?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D" target="_blank">University of Waterloo</a>! 💻
+          . I'm currently studying Computer Science @ <a href="https://www.google.com/maps/dir/Fredericton,+New+Brunswick/University+of+Waterloo,+200+University+Ave+W,+Waterloo,+ON+N2L+3G1/@45.5824289,-76.2330032,784030m/data=!3m2!1e3!4b1!4m14!4m13!1m5!1m1!1s0x4ca4220ba498fb2b:0xe7de2f297a415db4!2m2!1d-66.6431151!2d45.9635895!1m5!1m1!1s0x882bf6ad02edccff:0xdd9df23996268e17!2m2!1d-80.5448576!2d43.4722854!3e0!5m2!1e1!1e4?entry=ttu&g_ep=EgoyMDI1MDMyMy4wIKXMDSoASAFQAw%3D%3D" target="_blank">University of Waterloo</a>! 💻
         </p>
         <p>
           Currently: <br></br>
