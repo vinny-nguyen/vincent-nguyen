3e2a62bb2f8e508c9d30935ede58d03016ce61f1
Vincent Nguyen <phucvn166@gmail.com>
2025-03-28 15:37:01 -0400

    Added UW CS Webring Button above social media links on Footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,23 +35,58 @@ function SocialLinks() {
 export default function Footer() {
   return (
     <footer id="contact" className="block lg:mt-12 mt-8 pb-10 text-[#1C1C1C] dark:text-[#D4D4D4]">
-      <time>© {YEAR}</time>{" "}
-      <a
-        className="no-underline"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {metaData.title}
-      </a>
-      <style jsx>{`
+      {/* UW CS Webring Button: */}
+      <div className="flex items-center float-right gap-2 mb-4">
+        <a 
+        href="https://cs.uwatering.com/vinny-nguyen.vercel.app?nav=prev"
+        className="text-xl"
+        >←</a>
+        <a 
+          href="https://cs.uwatering.com/vinny-nguyen.vercel.app" 
+          target='_blank'
+          rel="noopener noreferrer">
+            {/* Light mode */}
+            <img
+              src="https://cs.uwatering.com/icon.black.svg"
+              alt="CS Webring"
+              style={{ width: "24px", height: "auto", opacity: 0.8 }}
+              className="dark:hidden"
+            />
+            {/* Dark mode */} 
+            <img
+              src="https://cs.uwatering.com/icon.white.svg"
+              alt="CS Webring (Dark)"
+              style={{ width: "24px", height: "auto", opacity: 0.8 }}
+              className="hidden dark:block"
+            />
+        </a>
+        <a 
+          href="https://cs.uwatering.com/vinny-nguyen.vercel.app?nav=next"
+          className="text-xl"
+        >→</a>
+      </div>
+
+      {/* Copyright + Social Media links: */}
+      <div className="flex items-center justify-between w-full">
+        <div className="text-sm">
+          <time>© {YEAR}</time>{" "}
+          <a className="no-underline" target="_blank" rel="noopener noreferrer">
+            {metaData.title}
+          </a>
+        </div>
+      
+        <style jsx>{`
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
             padding-bottom: 4rem;
           }
         }
-      `}</style>
+        `}</style>
+      
       <SocialLinks />
+      </div>
+      
     </footer>
   );
 }
