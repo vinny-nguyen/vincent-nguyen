36f40bbebbc705e092c6227bbebca8b0f4bfdb65
Vincent Nguyen <phucvn166@gmail.com>
2025-04-15 18:35:37 -0400

    changed buttons for theme-swtich + added rooster/cricket sounds

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "next-themes";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 import { FaCircleHalfStroke } from "react-icons/fa6";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const storageKey = 'theme-preference';
 
@@ -64,13 +65,22 @@ export const ThemeSwitch: React.FC = () => {
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
     localStorage.setItem(storageKey, newTheme);
     reflectPreference(newTheme);
+
+    if (newTheme == "dark") {
+      const CricketAudio = new Audio('/audio/crickettest.mp3');
+      CricketAudio.play();
+
+    } else {
+      const RoosterAudio = new Audio('/audio/roostertest.mp3');
+      RoosterAudio.play();
+    }
   };
 
   if (!mounted) {
     return (
-      <FaCircleHalfStroke
-        className="h-[14px] w-[14px] text-[#1c1c1c]"
-        aria-hidden="true"
+      <div
+        className="h-[14px] w-[14px]" // text-[#1c1c1c]
+        // aria-hidden="true"
       />
     );
   }
@@ -80,13 +90,25 @@ export const ThemeSwitch: React.FC = () => {
       id="theme-toggle"
       aria-label={`${currentTheme} mode`}
       onClick={toggleTheme}
-      className="flex items-center justify-center transition-opacity duration-300 hover:opacity-90"
+      className="
+      p-2 rounded-lg
+      flex items-center justify-center 
+      transition-opacity duration-300 
+      bg-white dark:bg-black
+      hover:bg-stone-200 dark:hover:bg-stone-800"
     >
-      <FaCircleHalfStroke
+      {currentTheme === "dark" ? (
+        <FiMoon className="h-[18px] w-[18px] text-[#D4D4D4]" />
+      ) : (
+
+        <FiSun className="h-[18px] w-[18px] text-[#1c1c1c]" />
+      )}
+
+      {/* <FaCircleHalfStroke
         className={`h-[14px] w-[14px] ${
           currentTheme === "dark" ? "text-[#D4D4D4]" : "text-[#1c1c1c]"
         }`}
-      />
+      /> */}
     </button>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,8 @@ export default function Page() {
           bg-stone-50 dark:bg-stone-800 shadow-lg 
             block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto
             hover:grayscale-0 cursor-pointer
-            transition-transform duration-300 hover:scale-105"
+            transition-transform duration-300 hover:scale-105
+            group-hover:opacity-0"
           // className="w-48 h-48 relative rounded-full border-2 border-stone-200 dark:border-stone-700 shadow-lg overflow-hidden bg-stone-50 dark:bg-stone-800"
           unoptimized
           width={160}
@@ -32,6 +33,17 @@ export default function Page() {
           priority
           onClick={OpenModal}
         />
+
+        {/* <Image
+            src="/pictures/vincentnguyenguitarzoom.png"
+            alt="Retro Vincent Nguyen"
+            className="rounded-full w-full h-full absolute inset-0 border-4 border-stone-200 dark:border-stone-700 bg-stone-50 dark:bg-stone-800 shadow-lg transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+            unoptimized
+            width={160}
+            height={160}
+            priority
+        /> */}
+
         <img className="
         absolute bottom-[-10px] right-[-10px] 
         sm:bottom-[-30px] sm:right-[-20px] 
@@ -74,8 +86,8 @@ export default function Page() {
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          Originally from <a href="https://www.google.com/maps/place/Fredericton,+NB/@45.9454324,-66.7486524,18635m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4ca4220ba498fb2b:0xe7de2f297a415db4!8m2!3d45.9635895!4d-66.6431151!16zL20vMDJ3NzA!5m2!1e1!1e4?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D" target="_blank">Fredericton, New Brunswick</a>
-          , I'm currently studying CS @ <a href="https://cs.uwaterloo.ca/" target="_blank">University of Waterloo</a>! 💻
+          Growing up in <a href="https://www.google.com/maps/place/Fredericton,+NB/@45.9454324,-66.7486524,18635m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4ca4220ba498fb2b:0xe7de2f297a415db4!8m2!3d45.9635895!4d-66.6431151!16zL20vMDJ3NzA!5m2!1e1!1e4?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D" target="_blank">Fredericton, New Brunswick</a>
+          , I'm currently studying CS @ <a href="https://cs.uwaterloo.ca/" target="_blank">University of Waterloo</a>.
         </p>
         <p>
           Currently: <br></br>
